d9179cb296d2778085d6a9704078db596e62fd0e
JhonJSC182 <jhonjsc182@hotmail.com>
2024-04-15 22:00:37 -0700

    css for singup/signin

--- frontend/src/components/Modal/SessionModal.jsx
+++ frontend/src/components/Modal/SessionModal.jsx
@@ -1,43 +1,46 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 import './SessionModal.css'
-// import { useDispatch } from 'react-redux'
+import { useDispatch } from 'react-redux'
+import { login, signup } from '../../store/session';
 
-//need to createUser & loginUser in the backend 'sessionReducer, then import it'
+//need to login & signup in the  'sessionReducer, then import it'
 
-const SessionModal = ({ modalState}) => {
-    // const dispatch = useDispatch();
-    // const [username, setUsername] = useState('');
-    // const [password, setPassword] = useState('');
+const SessionModal = ({ modalState }) => {
+    const dispatch = useDispatch();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-    // const handleSubmit = e => {
-    //     e.preventDefault();
-    //     if (modalState === 'signup') {
-    //         dispatch(createUser({username, password}))
-    //             .then(() => setModalState(null))
-    //     } else {
-    //         dispatch(loginUer({ username, password }))
-    //         .then(() => setModalState(null))
-    //     }
-    // };
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (modalState === 'signup') {
+            dispatch(signup({ email, username, password}))
+                .then(() => setModalState(null))
+        } else {
+            dispatch(login({ email, password }))
+            .then(() => setModalState(null))
+        }
+    };
 
     return(
         <div className='modal-background' >
-            {/* <div className='modal-content' > */}
+            <div className='modal-content' >
                 <h2>SessionModal: {modalState}</h2>
-                {/* <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit}>
                     <input 
+                        className='buttons'
                         placeholder='Username'
                         value={username}
                         onChange={e => setUsername(e.target.value)} 
                     />
                     <input 
+                        className='buttons'
                         placeholder='Password'
                         value={password}
                         onChange={e => setPassword(e.target.value)} 
                     />
-                    <input type="submit" value={modalState} />
-                </form> */}
-            {/* </div> */}
+                    <input className='click' type="submit" value={modalState} />
+                </form>
+            </div>
         </div>
     )
 
