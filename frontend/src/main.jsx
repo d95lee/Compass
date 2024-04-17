6b2ae69e37d5e55d355bae72525848f797a6bea5
Jerry Wang <ziyanwang1105@gmail.com>
2024-04-17 09:15:25 -0700

    finish event thunk action

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,18 @@ import App from './App';
 import './index.css';
 import configureStore from './store/store.js';
 import { fetchItineraries, fetchItinerary } from './store/itinerary.js';
+import  {createEvent, deleteEvent, updateEvent}  from './store/event.js';
 
 const store = configureStore();
 
-window.store = store;
-window.fetchItinerary = fetchItinerary;
-window.fetchItineraries = fetchItineraries;
+if (import.meta.env.MODE !== "production") {
+  window.store = store;
+  window.fetchItinerary = fetchItinerary;
+  window.fetchItineraries = fetchItineraries;
+  window.createEvent = createEvent
+  window.updateEvent = updateEvent
+  window.deleteEvent = deleteEvent
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
