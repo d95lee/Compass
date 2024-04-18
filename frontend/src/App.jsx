acd253234aadd2756cfd673ca6e51776368a7d3f
JhonJSC182 <jhonjsc182@hotmail.com>
2024-04-18 00:08:05 -0700

    put info in ItineraryForm

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -54,7 +54,7 @@ const router = createBrowserRouter([
       </>
   },
   {
-    path: 'form',
+    path: 'form/itinerary/:itineraryId',
     element: 
       <>
         <NavBar/>
--- frontend/src/components/ItineraryForm/ItineraryForm.jsx
+++ frontend/src/components/ItineraryForm/ItineraryForm.jsx
@@ -1,16 +1,90 @@
+import { useDispatch, useSelector } from 'react-redux'
 import './ItineraryForm.css'
+import { useParams } from 'react-router-dom';
+import { fetchItinerary, selectItinerary } from '../../store/itinerary';
+import { useEffect } from 'react';
 
 const ItineraryForm = () => {
+    const dispatch = useDispatch();
+    const { itineraryId } = useParams();
+    const itinerary = useSelector(selectItinerary(itineraryId))
+
+    useEffect(() => {
+        dispatch(fetchItinerary(itineraryId))
+    }, [dispatch, itineraryId])
+
     return (
         <div className='form-background'>
-            <div className='event-form'>
-                <h1>Events</h1>
-                <hr />
-                <div className='event-info'>
-                    <h4>Event title</h4>
-                    <input type="text" />
+
+<div className='event-form'>
+            <h1>Events title:{itinerary?.events[0]?.eventTitle} </h1>
+            <hr />
+
+            {itinerary?.events.map(event => (
+                
+                <div key={event._id} className='event-item'>
+                    <div className='event-start-time'>
+                        <h4>Start time</h4>
+                        <p>{event.startTime}</p>
+                    </div>
+
+                    <div className='event-end-time'>
+                        <h4>End time</h4>
+                        <p>{event.endTime}</p>
+                    </div>
+
+                    <div className='event-location'>
+                        <h4>Location</h4>
+                        <p>{event.location}</p>
+                    </div>
+
+                    <div className='event-cost'>
+                        <h4>Cost</h4>
+                        <p>{event.cost}</p>
+                    </div>
+
+                    <div className='event-description'>
+                        <h4>Description</h4>
+                        <p>{event.description}</p>
+                    </div>
                 </div>
-            </div>
+            ))}
+        </div>
+
+            {/* <div className='event-form'>
+                <h1>Events title: </h1>
+                <hr />
+
+                <div className='event-start-time'>
+                    <h4>Start time</h4>
+                    <p>{itinerary?.event[0]?.eventTitle}</p>
+                </div> */}
+
+                {/* <div className='event-end-time'>
+                    <h4>End time</h4>
+                    <p></p>
+                </div>
+
+                <div className='event-location'>
+                    <h4>Location</h4>
+                    <p></p>
+                </div>
+
+                <div className='event-cost'>
+                    <h4>Cost</h4>
+                    <p></p>
+                </div>
+
+                <div className='event-description'>
+                    <h4>Description</h4>
+                    <p></p>
+                </div> */}
+
+            {/* </div> */}
+
+
+
+
 
             <div className='transportation-form'>
                 <h1>Transportation</h1>
