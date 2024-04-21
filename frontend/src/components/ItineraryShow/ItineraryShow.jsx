98b084131199996998544ee56aba764dd2931c97
Caroline Zhang <cczhang1@yahoo.com>
2024-04-21 01:28:19 -0700

    add in like functionality to show page

--- frontend/src/components/ItineraryShow/ItineraryShow.jsx
+++ frontend/src/components/ItineraryShow/ItineraryShow.jsx
@@ -1,16 +1,21 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
-import { useParams, Link } from "react-router-dom"
+import { useParams, Link, useNavigate } from "react-router-dom"
 import { fetchItinerary, selectItinerary } from "../../store/itinerary"
-import { fetchUser } from "../../store/user"
+import { fetchUser, selectUser } from "../../store/user"
 import "./ItineraryShow.css"
 import TimelineBox from "./TimelineBox"
 import nyc from '../../../../assets/nyc-brooklyn-bridge.jpeg'
 import editbutton from '../../../../assets/edit-button.png'
+import { likeItinerary, unlikeItinerary } from "../../store/like"
+
 
 const ItineraryShow = () => {
     const dispatch = useDispatch()
     const { itineraryId } = useParams()
+    const navigate = useNavigate();
+    const [liked, setLiked] = useState(false);
+    const [heartColor, setHeartColor] = useState("#000000");
 
     useEffect(() => {
         dispatch(fetchItinerary(itineraryId));
@@ -22,8 +27,59 @@ const ItineraryShow = () => {
         dispatch(fetchUser(userId));
     }, [dispatch, userId])
 
-    console.log(userId, 'userId')
+    const user = useSelector(selectUser(userId));
 
+    // useEffect to check the like status of the current itinerary 
+    useEffect(() =>{
+            
+        const likeStatus = (user, itineraryId) => {
+            let liked = false;
+            user?.likes.forEach(like => {
+                if (itineraryId === like.itinerary) {
+                    liked = true;
+                    setHeartColor('#ff0000');
+                } 
+            })
+            return liked;
+        }
+        
+        setLiked(likeStatus(user, itineraryId));
+        console.log('liked');
+
+    }, [itineraryId, user, itinerary])
+
+    console.log(liked);
+    // helper function that takes (user, itineraryId)- returns boolean whether user has liked the itinerary or not
+// when you fetch the user object, 
+
+    // use timeline chronological sort on array from date key 
+    const handleLike = () => {
+        // on click - like it or unlike it 
+        if (!liked){
+           console.log('here');
+           dispatch(likeItinerary(itineraryId)); 
+           setHeartColor('#ff0000');
+        } else {
+            console.log('hello')
+            let likeIdValue;
+            user.likes.forEach(like =>{
+                if (like.itinerary === itineraryId) {
+                    likeIdValue = like._id
+                }
+            })
+            dispatch(unlikeItinerary(likeIdValue, itineraryId));
+            setHeartColor('#000000');
+        }
+    
+    }
+
+
+    const handleEditClick = () => {
+        navigate(`/itinerary/form/${itinerary?._id}`);
+    }
+// mongoDB database technically only stores likeId in the likes array of each user, but when we fetch it from the 
+// frontend, due to extra information added for the routes, we will have likes: [{likeId:xxx, itinerary:xxxx},... ]
+    // console.log(user, 'user');
     const itinerarySubobjectsArray = ( itinerary )=> {
         const newArray = [];  // event = { title: '', date: ''}
         itinerary?.events.map(event => (newArray.push({...event, 'type': 'event'})));
@@ -50,8 +106,8 @@ const ItineraryShow = () => {
     }
 
 
-    console.log(itinerarySubobjectsArray(itinerary), 'test array');
-    console.log(itinerary);    
+    // console.log(itinerarySubobjectsArray(itinerary), 'test array');
+    // console.log(itinerary);    
     return (
         <>
             <div className="itinerary-show-page-container">
@@ -63,19 +119,23 @@ const ItineraryShow = () => {
                     <div className='itinerary-show-page-header-text'>
                         <div className='itinerary-show-page-title'>{itinerary?.title.toUpperCase()}</div>  
                         <div className='itinerary-show-page-description'>{itinerary?.description.toUpperCase()}</div> 
-
+                        <div className='itinerary-show-page-description'>@{user?.username}</div> 
                     </div>
-                    <svg fill="#000000" height="23px" width="23px" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 471.701 471.701" xml:space="preserve">
-                        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                        <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
-                        <g id="SVGRepo_iconCarrier"> <g> 
-                        <path d="M433.601,67.001c-24.7-24.7-57.4-38.2-92.3-38.2s-67.7,13.6-92.4,38.3l-12.9,12.9l-13.1-13.1 c-24.7-24.7-57.6-38.4-92.5-38.4c-34.8,0-67.6,13.6-92.2,38.2c-24.7,24.7-38.3,57.5-38.2,92.4c0,34.9,13.7,67.6,38.4,92.3 l187.8,187.8c2.6,2.6,6.1,4,9.5,4c3.4,0,6.9-1.3,9.5-3.9l188.2-187.5c24.7-24.7,38.3-57.5,38.3-92.4 C471.801,124.501,458.301,91.701,433.601,67.001z M414.401,232.701l-178.7,178l-178.3-178.3c-19.6-19.6-30.4-45.6-30.4-73.3 s10.7-53.7,30.3-73.2c19.5-19.5,45.5-30.3,73.1-30.3c27.7,0,53.8,10.8,73.4,30.4l22.6,22.6c5.3,5.3,13.8,5.3,19.1,0l22.4-22.4 c19.6-19.6,45.7-30.4,73.3-30.4c27.6,0,53.6,10.8,73.2,30.3c19.6,19.6,30.3,45.6,30.3,73.3 C444.801,187.101,434.001,213.101,414.401,232.701z"></path> </g> </g></svg>
-                    <Link to={`/itinerary/form/${itinerary?._id}`}>
-                    <div className='itinerary-edit-button-box'>
+                    
+                    <div></div>
 
-                        <img className='itinerary-edit-button' src={editbutton} />
+                        <svg xmlns="http://www.w3.org/2000/svg" fill={heartColor} x="0px" y="0px" height="23px" width="23px" viewBox="0 0 122.88 107.41" onClick={handleLike}><g>
+                            <path d="M60.83,17.19C68.84,8.84,74.45,1.62,86.79,0.21c23.17-2.66,44.48,21.06,32.78,44.41 c-3.33,6.65-10.11,14.56-17.61,22.32c-8.23,8.52-17.34,16.87-23.72,23.2l-17.4,17.26L46.46,93.56C29.16,76.9,0.95,55.93,0.02,29.95 C-0.63,11.75,13.73,0.09,30.25,0.3C45.01,0.5,51.22,7.84,60.83,17.19L60.83,17.19L60.83,17.19z"/></g>
+                        </svg>
+                        <div className="likes-number-text">{itinerary?.likes} likes</div>
+                    
+                    
+                    
+                   
+                    <div className='itinerary-edit-button-box'>
+                        <img className='itinerary-edit-button' src={editbutton} onClick={handleEditClick} />
                     </div>                    
-                    </Link>
+                    
                 </div>
 
 
