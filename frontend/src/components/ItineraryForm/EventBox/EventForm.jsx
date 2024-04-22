e2c35e982a7470f1078fc235d8d2dd96ae275dbe
Caroline Zhang <cczhang1@yahoo.com>
2024-04-22 10:25:20 -0700

    add form changes and other css changes

--- frontend/src/components/ItineraryForm/EventBox/EventForm.jsx
+++ frontend/src/components/ItineraryForm/EventBox/EventForm.jsx
@@ -66,73 +66,73 @@ const EventForm = ({itinerary, event, eventModalState, setEventModalState, setEv
     return (
         <>
             <div className="modal-background" onClick={handleClose}>
-                <div className="modal-content" onClick={e => e.stopPropagation()}>
+                <div className="add-event-modal-content" onClick={e => e.stopPropagation()}>
                     <h3>{eventModalState === "Add" ? 'Add Event' : 'Edit Event'}</h3>
                     <form className="event-form-modal" onSubmit={handleSubmit}>
-                        <label>Event Title
-                            <input type="text"
+                        <label>
+                            <input className='timeline-form-input' type="text"
                             value={eventTitle}
                             onChange={(e) => setEventTitle(e.target.value)}
                             placeholder="Event Title"
                             />
                         </label>
 
-                        <label>Start Time
-                            <input type="text"
+                        <label>
+                            <input className='timeline-form-input' type="text"
                             value={startTime}
                             onChange={(e) => setStartTime(e.target.value)}
                             placeholder="Start time 12:00"
                             />
                         </label>
 
-                        <label>End Time
-                            <input type="text"
+                        <label>
+                            <input className='timeline-form-input' type="text"
                             value={endTime}
                             onChange={(e) => setEndTime(e.target.value)}
                             placeholder="Start time 12:00"
                             />
                         </label>
 
-                        <label>Date
-                            <input type="text"
+                        <label>
+                            <input className='timeline-form-input' type="text"
                             value={date}
                             onChange={(e) => setDate(e.target.value)}
                             placeholder="Date 01/01/2020"
                             />
                         </label>
 
-                        <label>Location
-                            <input type="text"
+                        <label>
+                            <input className='timeline-form-input' type="text"
                             value={location}
                             onChange={(e) => setLocation(e.target.value)}
                             placeholder="Location"
                             />
                         </label>
 
-                        <label>Description
-                            <input type="text"
+                        <label>
+                            <input className='timeline-form-input' type="text"
                             value={description}
                             onChange={(e) => setDescription(e.target.value)}
                             placeholder="Description (optional)"
                             />
                         </label>
 
-                        <label>Category
-                            <input type="text"
+                        <label>
+                            <input className='timeline-form-input' type="text"
                             value={category}
                             onChange={(e) => setCategory(e.target.value)}
                             placeholder="Category"
                             />
                         </label>
 
-                        <label>Cost
-                            <input type="text"
+                        <label>
+                            <input className='timeline-form-input' type="text"
                             value={cost}
                             onChange={(e) => setCost(e.target.value)}
                             placeholder="Cost"
                             />
                         </label>
-                        <input type="submit" value={"Save"} />
+                        <input className='timeline-form-input-submit' type="submit" value={"Save"} />
                     </form>
                 </div>
             </div>
--- frontend/src/components/ItineraryForm/LivingBox/LivingBox.jsx
+++ frontend/src/components/ItineraryForm/LivingBox/LivingBox.jsx
@@ -12,17 +12,25 @@ const LivingBox = ({itinerary}) => {
 
     const sortedLiving = livingSort(itinerary)
     const createLivingCalendar = (sortedLiving) => {
-        // can't use for loop bc we need to return the subcomponent each time
-
         return Object.values(sortedLiving).map((ele, idx) => (<LivingDateBox key={idx} livingArray={ele} date={Object.keys(sortedLiving)[idx]} setLivingModalState={setLivingModalState} setLiving={setLiving}/>))
     }
 
     return(
         <>
             <div className="living-form-box">
-                <h3>Living Accommodation</h3>
-                <button onClick={e=>setLivingModalState('Add')}>Add</button>
-                {sortedLiving && createLivingCalendar(sortedLiving)}
+                <div className="event-form-box-header">
+                    <h3>Living Accommodation</h3>
+                    <div>
+                        <svg className="event-form-add-button" fill="#9c9696" height='20px' width='20px' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 45.402 45.402" onClick={e => setLivingModalState('Add')}>
+                            <path d="M41.267,18.557H26.832V4.134C26.832,1.851,24.99,0,22.707,0c-2.283,0-4.124,1.851-4.124,4.135v14.432H4.141 c-2.283,0-4.139,1.851-4.138,4.135c-0.001,1.141,0.46,2.187,1.207,2.934c0.748,0.749,1.78,1.222,2.92,1.222h14.453V41.27 c0,1.142,0.453,2.176,1.201,2.922c0.748,0.748,1.777,1.211,2.919,1.211c2.282,0,4.129-1.851,4.129-4.133V26.857h14.435 c2.283,0,4.134-1.867,4.133-4.15C45.399,20.425,43.548,18.557,41.267,18.557z"></path>
+                        </svg>
+                    </div>
+                </div>
+                    
+                <div className="event-days-container">
+                    {sortedLiving && createLivingCalendar(sortedLiving)}
+                </div>
+               
 
             </div>
             {livingModalState && <LivingForm itinerary = {itinerary}
