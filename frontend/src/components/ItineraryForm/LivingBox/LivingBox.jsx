cbde19055ed120ab20e0c8a72cf88f4c8e1794d4
Jerry Wang <ziyanwang1105@gmail.com>
2024-04-21 21:27:38 -0700

    refactor form for calender look

--- frontend/src/components/ItineraryForm/LivingBox/LivingBox.jsx
+++ frontend/src/components/ItineraryForm/LivingBox/LivingBox.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import LivingItem from "./LivingItem";
 import LivingForm from "./LivingForm";
+import { livingSort } from "../../../utils/calenderSort";
+import LivingDateBox from "./LivingDateBox";
 
 
 const LivingBox = ({itinerary}) => {
@@ -8,13 +10,19 @@ const LivingBox = ({itinerary}) => {
     const [livingModalState, setLivingModalState] = useState(null)
     const [living, setLiving] = useState({})
 
+    const sortedLiving = livingSort(itinerary)
+    const createLivingCalendar = (sortedLiving) => {
+        // can't use for loop bc we need to return the subcomponent each time
+
+        return Object.values(sortedLiving).map((ele, idx) => (<LivingDateBox key={idx} livingArray={ele} date={Object.keys(sortedLiving)[idx]} setLivingModalState={setLivingModalState} setLiving={setLiving}/>))
+    }
+
     return(
         <>
             <div className="living-form-box">
                 <h3>Living Accommodation</h3>
                 <button onClick={e=>setLivingModalState('Add')}>Add</button>
-                {livings && livings.map((el,idx) => (<LivingItem key={idx} living={el}
-                 setLivingModalState={setLivingModalState} setLiving={setLiving}/>))}
+                {sortedLiving && createLivingCalendar(sortedLiving)}
 
             </div>
             {livingModalState && <LivingForm itinerary = {itinerary}
