d10c3c70fbf63737c05607f1751ed3fb08a03a87
David <euntek95@gmail.com>
2024-04-22 02:12:37 -0700

    add profile pics

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -2,88 +2,69 @@ import { useEffect, useState } from 'react'
 import './Profile.css'
 import { useDispatch, useSelector } from 'react-redux'
 import { useParams } from 'react-router-dom'
-import { fetchItineraries, selectItineraries, selectLikedItinerary } from '../../store/itinerary';
+import { fetchItineraries, selectItineraries } from '../../store/itinerary';
 import { fetchUser, selectUser } from '../../store/user'
 import ItineraryItem from '../ItineraryItem/ItineraryItem';
 import { removeGlobe } from '../Globe/Globe';
-import BioModal from './BioModal';
 
 
 const Profile = () => {
     const dispatch = useDispatch();
     const { userId } = useParams();
 
-    const [bioState, setBioState] = useState(null);
+    // const [itinerary, setItinerary] = useState('myItinerary');
 
 
     const itineraries = useSelector(selectItineraries);
-
-
+    // console.log(itineraries, 'itineraries');
     useEffect(() => {
-        dispatch(fetchItineraries(user));
+        dispatch(fetchItineraries());
     }, [dispatch])
 
-    
+
     const user = useSelector(selectUser(userId));
+
     useEffect(() => {
         dispatch(fetchUser(userId));
     }, [dispatch, userId])
-    
-    
-        const likeItineraries = useSelector(selectLikedItinerary(user))
-        console.log(likeItineraries, 'LIKED')
 
+
+    // if (itinerary === 'myItinerary') {
+
+    // }
     removeGlobe()
 
-
     return (
-        <>
-        
         <div className='profile-background'>
-        {bioState && (
-                <BioModal 
-                    bioState={bioState} 
-                    setBioState={setBioState}
-                />
-            )}
+
             <img className='background-img' src="https://wallpapers.com/images/featured/widescreen-3ao0esn9qknhdudj.jpg" alt="" />
             <div className='pic-div'>
-                {user?.profileImageUrl && (<img className='user-img' src={user.profileImageUrl}/>)}
+                {user?.profileImageUrl && (<img className='user-img' src={user?.profileImageUrl}/>)}
             </div>
 
             <div className='user-info'>
                 <h3>Bio</h3>
-                <p className='user-bio'>{user?.bio}</p>
-
+                <p>{user?.bio}</p>
             </div>
-            <button onClick={() => setBioState(true)} className='profile-edit-button'>Edit</button>
-  
+            <button className='profile-edit-button'>Edit</button>
 
             <div className='profile-itineraries'>
-                {/* <div className='itinerary-buttons'>
+                <div className='itinerary-buttons'>
                     <button className='my-button' onClick={() => setItinerary('myItinerary')}>My itineraries</button>
                     <button className='liked-button' onClick={() => setItinerary('likedItinerary')}>Liked itineraries</button>
-                </div> */}
+                </div>
                 <div className='blocks'>
                     <div className='my-itineraries'>
-                        <h2 className='header-display'>My itineraries</h2>
                             <div className='itinerary-index'>
                             {/* cannot directly pass in object to a sub component as prop, unless it's in an array */}
                             {Object.values(itineraries).map((itinerary, idx) => <ItineraryItem key={idx} itinerary={itinerary} />)}
+
                             </div>
                     </div>
-
-                    <div className='like-itineraries'>
-                        <h2 className='header-display'>Liked itineraries</h2>
-                        <div className='itinerary-index'>
-                        {likeItineraries.map((itinerary, idx) => <ItineraryItem key={idx} itinerary={itinerary} />)}
-                        </div>
-                    </div>
-
+                
                 </div>
             </div>
         </div>
-        </>
     )
 }
 
